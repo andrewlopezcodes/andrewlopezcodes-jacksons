835843c013c86bdb78f547f6e8dde640827dae9e
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-07-08 14:25:30 -0400

    finished collapse menu

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("main-doc").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main-doc").style.marginLeft = "30px";
+}
